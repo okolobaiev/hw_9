2caf8f64b3043cfd154c36a674d309565abcb2c3
okolobaiev <okolobaiev@gmail.com>
2023-09-02 00:10:38 +0300

    add decorator

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 phone_book = {}
 
 
-def input_error():
-    pass
+def input_error(func):
+    def inner(*param: tuple) -> str:
+        try:
+            return func(*param)
+        except KeyError:
+            return "Enter user name"
+        except ValueError:
+            return "Give me name and phone please"
+        except IndexError:
+            return "Give me name and phone please"
+
+    return inner
 
 
-def save_name_and_phone(name: str, phone: str) -> str:
-    phone_book[name] = phone
+@input_error
+def save_name_and_phone(*param: tuple) -> str:
+    list_of_param = param[0]
+    phone_book[list_of_param[0]] = list_of_param[1]
+    return f"Done"
+
+
+@input_error
+def find_phone_by_name(*param: tuple) -> str:
+    list_of_param = param[0]
+    return phone_book[list_of_param[0]]
+
+
+@input_error
+def change_phone(*param: tuple) -> str:
+    list_of_param = param[0]
+    phone_book[list_of_param[0]] = list_of_param[1]
     return "Done"
 
 
-def find_phone_by_name(name: str) -> str:
-    return phone_book.get(name)
-
-
-def change_phone(name: str, phone: str) -> str:
-    phone_book[name] = phone
-    return "Done"
-
-
+@input_error
 def all_phones_str() -> str:
     result = ""
     for k, v in phone_book.items():
@@ -29,20 +46,20 @@ def all_phones_str() -> str:
 def command_parcer(input_text: str) -> str:
     words = input_text.split(" ")
 
-    if len(words) == 1 and words[0].lower() == "hello":
+    if words[0].lower() == "hello":
         return "How can I help you?"
-    elif len(words) == 3 and words[0].lower() == "add":
-        return save_name_and_phone(words[1], words[2])
-    elif len(words) == 3 and words[0].lower() == "change":
-        return change_phone(words[1], words[2])
-    elif len(words) == 2 and words[0].lower() == "phone":
-        return find_phone_by_name(words[1])
+    elif words[0].lower() == "add":
+        return save_name_and_phone(words[1::])
+    elif words[0].lower() == "change":
+        return change_phone(words[1::])
+    elif words[0].lower() == "phone":
+        return find_phone_by_name(words[1::])
     elif len(words) == 2 and words[0].lower() == "show" and words[1].lower() == "all":
         return all_phones_str()
     elif (
         (len(words) == 2 and words[0].lower() == "good" and words[1].lower() == "bye")
-        or (len(words) == 1 and words[0].lower() == "close")
-        or (len(words) == 1 and words[0].lower() == "exit")
+        or (words[0].lower() == "close")
+        or (words[0].lower() == "exit")
     ):
         return "exit"
     else:
